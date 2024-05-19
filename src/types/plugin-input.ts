1c6928141897029533eb872f77dbbdd130d285fe
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-05-19 03:03:57 +0100

    chore: add types and adapters

---
+++ src/types/plugin-input.ts
@@ -0,0 +1,32 @@
+import { EmitterWebhookEvent as WebhookEvent, EmitterWebhookEventName as WebhookEventName } from "@octokit/webhooks";
+import { SupportedEvents } from "./context";
+import { StaticDecode, Type as T } from "@sinclair/typebox";
+
+export interface PluginInputs<T extends WebhookEventName = SupportedEvents> {
+  stateId: string;
+  eventName: T;
+  eventPayload: WebhookEvent<T>["payload"];
+  settings: PluginSettings;
+  authToken: string;
+  ref: string;
+}
+
+export const pluginSettingsSchema = T.Object({
+  labels: T.Object(
+    {
+      time: T.Array(T.String(), { default: [] }),
+      priority: T.Array(T.String(), { default: [] }),
+    },
+    { default: {} }
+  ),
+  basePriceMultiplier: T.Number({ default: 1 }),
+  publicAccessControl: T.Object(
+    {
+      setLabel: T.Boolean({ default: false }),
+      fundExternalClosedIssue: T.Boolean({ default: false }),
+    },
+    { default: {} }
+  ),
+});
+
+export type PluginSettings = StaticDecode<typeof pluginSettingsSchema>;
